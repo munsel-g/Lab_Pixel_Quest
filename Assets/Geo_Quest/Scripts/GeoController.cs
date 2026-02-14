3364fcfcedc04dea8d9b3ba4d1d6b6c30675d3db
munsel-g <munseldedon@gmail.com>
2026-02-14 16:29:05 -0500

    C# script added

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string variable1 = "Hello "; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variable2 = "World";
+        Debug.Log(variable1 + variable2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
